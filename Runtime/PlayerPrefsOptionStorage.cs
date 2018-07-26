2e7f6154c9c4e7d16a79f4ffae281e7846f39694
James Liu <contact@jamessliu.com>
2018-07-26 03:09:00 -0700

    Update to new version

---
+++ Runtime/PlayerPrefsOptionStorage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HouraiTeahouse.Options {
+
+public class PlayerPrefsOptionsStorage : IOptionsStorage {
+
+  public bool IsOptionSet(string path) => PlayerPrefs.HasKey(path);
+
+  public void SaveOption(string path, float value) {
+    Debug.Log($"[PlayerPrefs] Saved Option \"{path}\": {value}");
+    PlayerPrefs.SetFloat(path, value);
+  }
+
+  public float GetOption(string path) {
+    var value = PlayerPrefs.GetFloat(path);
+    Debug.Log($"[PlayerPrefs] Loaded Option \"{path}\": {value}");
+    return value;
+  }
+
+  public void SaveChanges() {
+    PlayerPrefs.Save();
+    Debug.Log($"[PlayerPrefs] Flushed changes to disk.");
+  }
+
+}
+
+}
